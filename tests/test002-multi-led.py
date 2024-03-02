7301a5d2fd7cd6b6c589630770d63d690297a5c3
mkoterski <matthiaskoterski@gmail.com>
2024-03-02 11:00:37 +0100

    Update test002-multi-led.py

--- tests/test002-multi-led.py
+++ tests/test002-multi-led.py
@@ -7,15 +7,34 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
+print("T E S T   0 0 2   -   M U L T I   L E D")
+
 GPIO.output(17, True)
-time.sleep(3)
+
+print("R E D")
+
+time.sleep(5)
 GPIO.output(17, False)
 time.sleep(1)
 GPIO.output(18, True)
-time.sleep(3)
+
+print("Y E L L O W")
+
+time.sleep(5)
 GPIO.output(18, False)
 time.sleep(1)
 GPIO.output(22, True)
-time.sleep(3)
+
+print("G R E E N")
+
+time.sleep(5)
 GPIO.output(22, False)
 time.sleep(1)
+GPIO.output(23, True)
+
+print("W H I T E")
+
+time.sleep(5)
+GPIO.output(23, False)
+time.sleep(1)
+GPIO.cleanup()
